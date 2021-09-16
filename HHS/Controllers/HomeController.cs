b400af95ce95f28727f7b7bcefa58904cddb48d1
AGhasemzad <ali.ghasemzad@gmail.com>
2021-09-17 00:11:09 +0430

    add jobinja

--- HHS/Controllers/HomeController.cs
+++ HHS/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using HHS.Models;
+using HtmlAgilityPack;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -20,7 +21,30 @@ namespace HHS.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            List<JobInJaModel> jobs = new List<JobInJaModel>();
+
+            HtmlDocument html = new HtmlDocument();
+            HtmlWeb web = new HtmlWeb();
+
+            html = web.Load("https://jobinja.ir/jobs");
+
+            var data = html.DocumentNode.Descendants("ul").Where(x => x.GetAttributeValue("class", "").Equals("o-listView__list c-jobListView__list")).ToList()[0];
+            var data2 = data.Descendants("li").Where(x => x.GetAttributeValue("class", "").Trim().StartsWith("o-listView__item o-listView__item--hasIndicator c-jobListView__item")).ToList();
+
+            foreach (var item in data2)
+            {
+                var Title = item.Descendants("a").Where(x => x.GetAttributeValue("class", "").Equals("c-jobListView__titleLink")).First().InnerHtml;
+                var Image = item.Descendants("img").Where(x => x.GetAttributeValue("class", "").Equals("o-listView__itemIndicatorImage")).Select(e => e.GetAttributeValue("src", null)).First();
+                var JobFeatures = item.Descendants("li").Where(x => x.GetAttributeValue("class", "").Equals("c-jobListView__metaItem")).ToList();
+                var Company = JobFeatures[0].Descendants("span").First().InnerHtml;
+                var Location = JobFeatures[1].Descendants("span").First().InnerHtml;
+                var ContractType = JobFeatures[2].Descendants("span").First().Descendants("span").First().InnerHtml.Replace("&zwnj;", " ");
+
+                JobInJaModel job = new JobInJaModel(Title, Image, Company, Location, ContractType);
+                jobs.Add(job);
+            }
+
+            return View(jobs);
         }
 
         public IActionResult Privacy()
---
+++ HHS/Models/JobInJaModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HHS.Models
+{
+    public class JobInJaModel
+    {
+        public JobInJaModel()
+        {
+        }
+
+        public JobInJaModel(
+            string title,
+            string image,
+            string company,
+            string location,
+            string contractType)
+        {
+            Title = title;
+            Image = image;
+            Company = company;
+            Location = location;
+            ContractType = contractType;
+        }
+
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string Company { get; set; }
+        public string Location { get; set; }
+        public string ContractType { get; set; }
+    }
+}
